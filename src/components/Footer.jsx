11c62900f74fb4f27647514b05b3c1bdd1cc0c24
Seif Otefa <120288463+seifotefa@users.noreply.github.com>
2025-07-11 01:56:54 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
           <a href="https://x.com/usejinsa"  target="_blank"  rel="noopener noreferrer" className="hover:text-[#6347f1] text-xl transition">Twitter</a>
           <a href="https://linkedin.com/company/jinsaco/" target="_blank"  rel="noopener noreferrer" className="hover:text-[#6347f1] text-xl transition">LinkedIn</a>
           <a href="https://instagram.com/jinsa.co" target="_blank"  rel="noopener noreferrer" className="hover:text-[#6347f1] text-xl transition">Instagram</a>
-          <a href="#" target="_blank"  rel="noopener noreferrer" className="hover:text-[#6347f1] text-xl transition">Contact</a>
+          <a href="mailto:usejinsaco@gmail.com" target="_blank"  rel="noopener noreferrer" className="hover:text-[#6347f1] text-xl transition">Contact</a>
         </div>
         <p className="text-xl">© 2025 Jinsa.</p>
       </footer>
